81b787951cf2afa161fdabc90832340e439968c2
Reza Shirazian <reza.shirazian@gmail.com>
2015-06-20 23:41:54 -0700

    Initial Commit

---
+++ Strings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Strings
+//
+//  Created by Reza Shirazian on 2015-06-20.
+//  Copyright (c) 2015 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
